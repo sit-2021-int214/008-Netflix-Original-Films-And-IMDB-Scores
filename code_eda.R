e044d555faca762ba5f9c2c7950a964e91c3d7e7
pphaiaiai <tarathep.siripis@mail.kmutt.ac.th>
2021-10-15 00:10:40 +0700

    update code_eda.R

---
+++ code_eda.R
@@ -0,0 +1,48 @@
+library(dplyr)
+library(stringr)
+library(tidyr)
+library(assertive)
+library(readr)
+
+# Step 1: Load data
+Netflix_IMDB <- read_csv("https://raw.githubusercontent.com/sit-2021-int214/008-Netflix-Original-Films-And-IMDB-Scores/main/NetflixOriginals.csv")
+
+# Step 2: Change to tibble
+Netflix_IMDB <- as_tibble(Netflix_IMDB)
+glimpse(Netflix_IMDB)
+
+# Step 3: TransformData
+
+
+# Cleaning Data
+Netflix_IMDB$Title <- Netflix_IMDB$Title %>% str_remove("???")
+Netflix_IMDB <- Netflix_IMDB %>% rename(IMDB_Score=`IMDB Score`)
+
+Netflix_IMDB$Genre <- as.factor(Netflix_IMDB$Genre)
+
+# 1 
+Netflix_maxIMDB <- data.frame(Netflix_IMDB %>% filter(`IMDB Score` == max(`IMDB Score`)))
+as_tibble(Netflix_maxIMDB)
+
+# 2
+GenreIMDBmean <- data.frame(Netflix_IMDB %>% group_by(Genre) %>% summarise_at(vars(`IMDB Score`), list(meanIMDB = mean)))
+as_tibble(GenreIMDBmean)
+
+# 3
+movie_language <- data.frame(Netflix_IMDB %>%count(Language))
+as_tibble(movie_language)
+
+# 4
+meanRuntime <- data.frame(Netflix_IMDB$Runtime %>% mean())
+as_tibble(meanRuntime)
+
+# 5
+Netflix_IMDB %>% select(Title,IMDB_Score) %>% filter(Netflix_IMDB$IMDB_Score == min(Netflix_IMDB$IMDB_Score)) 
+Netflix_IMDB %>% select(Title,IMDB_Score) %>% filter(Netflix_IMDB$IMDB_Score == max(Netflix_IMDB$IMDB_Score))
+distanceneIMDB <- max(Netflix_IMDB$IMDB_Score)-min(Netflix_IMDB$IMDB_Score)
+as_tibble(distanceneIMDB)
+
+# 6
+quantityGenre <- Netflix_IMDB %>% count(Genre)
+as_tibble(quantityGenre)
+max(quantityGenre)
