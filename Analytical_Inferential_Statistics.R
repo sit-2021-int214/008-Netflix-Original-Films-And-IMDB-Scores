d6eea21ba85350258d4771d4f53395f1b13a4dab
Teeradet Panklai <teeradet.bigkung@mail.kmutt.ac.th>
2021-12-11 23:29:19 +0700

    add Analytical_Inferential_Statistics.R

---
+++ Analytical_Inferential_Statistics.R
@@ -0,0 +1,34 @@
+n1 <- 125
+n2 <- 183
+sd1 <- 33.7
+sd2 <- 28.33 
+xbar1 <- 86.85  
+xbar2 <- 94.99
+u0 <- 15
+
+#h0: mue1-mue2 >= 15   ,ha: mue1-mue2 <  15
+
+alpha <- 0.05
+
+
+z <- (xbar1 - xbar2) / (sqrt(sd1^2/n1+sd2^2/n2));z
+
+# P-value approach
+pvalue <- pnorm(z); pvalue
+
+# Critical Value approach
+zalpha <- qnorm(alpha);zalpha
+
+# Using p-value approach
+if(pvalue<=alpha){
+  print("Reject H0")
+}else{
+  print("Accept H0")
+}
+
+# Using critical value
+if(z<=zalpha){
+  print("Reject H0")
+}else{
+  print("Accept H0")
+}
